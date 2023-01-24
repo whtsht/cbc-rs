9bca55e8fab6c202ae6ee3dbe6f8f778918598be
whtsht <whiteshirt0079@gmail.com>
2023-01-24 18:21:04 +0900

    feat(resolver): function callable

--- src/resolve/expr.rs
+++ src/resolve/expr.rs
@@ -1,23 +1,126 @@
+use crate::node::def::def_var::Var;
+use crate::node::unary::SuffixOp;
 use crate::node::{def::DefNode, stmt::StmtNode, Node};
 use crate::node::{expr::ExprNode, primary::PrimaryNode, term::TermNode, unary::UnaryNode};
 
-use super::variable_scope::ResolverError;
+use super::variable_scope::{Entity, ResolverError};
 
-pub fn assignmentable(expr: &ExprNode) -> Result<(), ResolverError> {
-    match expr {
-        ExprNode::Assign { term, .. } | ExprNode::AssignOp { term, .. } => {
-            if !is_variable_term(term) {
-                Err(ResolverError {
-                    message: format!(
-                        "invalid expression: LHS cannot be assigned.\n    {:?}",
-                        expr
-                    ),
-                })?;
-            }
+pub fn dereference_check(nodes: &Vec<Node>) -> Result<(), ResolverError> {
+    for node in nodes {
+        match node {
+            Node::Def(def) => match def.as_ref() {
+                DefNode::Fun { block, .. } => {
+                    for stmt in block {
+                        match stmt {
+                            StmtNode::Expr(expr) => {
+                                if !assiment_check(&expr) {
+                                    Err(ResolverError {
+                                        message: format!(
+                                            "invalid expression: LHS cannot be assigned.\n    {:?}",
+                                            expr
+                                        ),
+                                    })?;
+                                }
+                                if !callable_check(&expr) {
+                                    Err(ResolverError {
+                                        message: format!("invalid expression: {:?}", expr),
+                                    })?;
+                                }
+                            }
+                            StmtNode::DefVars(defvar) => {
+                                for var in defvar.vars.iter() {
+                                    match var {
+                                        Var::Init { expr, .. } => {
+                                            if !assiment_check(&expr) {
+                                                Err(ResolverError {
+                                                    message: format!(
+                                            "invalid expression: LHS cannot be assigned.\n    {:?}",
+                                            expr
+                                        ),
+                                                })?;
+                                            }
+                                            if !callable_check(&expr) {
+                                                Err(ResolverError {
+                                                    message: format!(
+                                                        "invalid expression: {:?}",
+                                                        expr
+                                                    ),
+                                                })?;
+                                            }
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
         }
-        _ => {}
     }
     Ok(())
+}
+
+pub fn callable_check(expr: &ExprNode) -> bool {
+    match expr {
+        ExprNode::Term(term) => callable_term(term),
+        ExprNode::Assign { term, expr } | ExprNode::AssignOp { term, expr, .. } => {
+            callable_term(term) && callable_check(expr)
+        }
+        ExprNode::BinaryOp { lhs, rhs, .. } => callable_check(&lhs) && callable_check(&rhs),
+        ExprNode::TernaryOp { lhs, mhs, rhs, .. } => {
+            callable_check(&lhs) && callable_check(&mhs) && callable_check(&rhs)
+        }
+    }
+}
+
+pub fn callable_term(term: &TermNode) -> bool {
+    match term {
+        TermNode::Unary(unary) => callable_uanry(&unary),
+        TermNode::Cast(_, term) => callable_term(term.as_ref()),
+    }
+}
+
+pub fn callable_uanry(unary: &UnaryNode) -> bool {
+    match unary {
+        UnaryNode::Suffix(_, suffix) => match suffix.as_ref() {
+            SuffixOp::CallFu(_, _, entity) => {
+                if let Some(entity) = entity.as_ref() {
+                    callable_entity(entity)
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
+pub fn callable_entity(entity: &Entity) -> bool {
+    match entity {
+        Entity::Function { .. } => true,
+        Entity::Variable { _type, init, .. } => {
+            if let Some(expr) = init {
+                callable_check(expr)
+            } else {
+                false
+            }
+        }
+        _ => false,
+    }
+}
+
+pub fn assiment_check(expr: &ExprNode) -> bool {
+    match expr {
+        ExprNode::Assign { term, expr } | ExprNode::AssignOp { term, expr, .. } => {
+            return is_variable_term(term) && assiment_check(expr);
+        }
+        _ => true,
+    }
 }
 
 pub fn is_variable_term(term: &TermNode) -> bool {
@@ -43,28 +146,6 @@ pub fn is_variable_primary(primary: &PrimaryNode) -> bool {
     }
 }
 
-pub fn dereference_check(nodes: &Vec<Node>) -> Result<(), ResolverError> {
-    for node in nodes {
-        match node {
-            Node::Def(def) => match def.as_ref() {
-                DefNode::Fun { block, .. } => {
-                    for stmt in block {
-                        match stmt {
-                            StmtNode::Expr(expr) => {
-                                assignmentable(&expr)?;
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-                _ => {}
-            },
-            _ => {}
-        }
-    }
-    Ok(())
-}
-
 #[test]
 fn test_validation() {
     use super::variable_scope::{gen_scope_toplevel, Scope};
@@ -80,8 +161,41 @@ fn test_validation() {
     .unwrap();
     let scope =
         gen_scope_toplevel(&mut nodes, Rc::new(Scope::default()), Weak::new(), false).unwrap();
-
     let _scope_tree = gen_scope_toplevel(&mut nodes, scope, Weak::new(), true).unwrap();
 
+    assert!(dereference_check(&nodes).is_err());
+
+    let mut nodes = crate::node::parse(
+        r#"
+        void main(void) {
+            int a = 0;
+            int b = a;
+            int c = b(2);
+        }
+           "#,
+    )
+    .unwrap();
+    let scope =
+        gen_scope_toplevel(&mut nodes, Rc::new(Scope::default()), Weak::new(), false).unwrap();
+    let _scope_tree = gen_scope_toplevel(&mut nodes, scope, Weak::new(), true).unwrap();
+    assert!(dereference_check(&nodes).is_err());
+
+    let mut nodes = crate::node::parse(
+        r#"
+        int one(void) {
+            return 1;
+        }
+        void main(void) {
+            // ok
+            int a = one();
+            // error
+            int b = "one"();
+        }
+           "#,
+    )
+    .unwrap();
+    let scope =
+        gen_scope_toplevel(&mut nodes, Rc::new(Scope::default()), Weak::new(), false).unwrap();
+    let _scope_tree = gen_scope_toplevel(&mut nodes, scope, Weak::new(), true).unwrap();
     assert!(dereference_check(&nodes).is_err());
 }
